d82c2b1d5ef3ce4cc8b71a89437d48fdb11cbbc8
Syul968 <syul.pgn@gmail.com>
2018-08-28 18:20:08 -0500

    Initial commit. Added basic statistical operations on class Statistics

---
+++ Statistics.java
@@ -0,0 +1,142 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.*;
+
+public class Statistics {
+	public static int sum(int[] data) {
+		int sum = 0;
+		
+		for(int i = 0; i < data.length; i++)
+			sum += data[i];
+		
+		return sum;
+	}
+	
+	public static double sqrt(double n) {
+		double root = n / 2.0;	//	Initial root guess
+		
+		if(n == 0.0)
+			return 0.0;
+		if(n < 0.0) {
+			System.out.println("Negative numbers not allowed");
+			return 0.0;
+		}
+		
+		do {
+			root = (root + n / root) / 2.0;	//	Newton's root iteration
+		} while(root * root - n >= 0.0001);
+		
+		return root;
+	}
+	
+	public static HashMap<Integer, Integer> frequencies(int[] data) {
+		HashMap<Integer, Integer> frequencies = new HashMap<Integer, Integer>();
+		
+		for(int i = 0; i < data.length; i++) {
+			if(frequencies.containsKey(new Integer(data[i]))) {
+//				System.out.println(data[i] + " already in collection");
+				frequencies.put(data[i], new Integer(frequencies.get(data[i]) + 1));
+			} else {
+//				System.out.println(data[i] + " not in collection");
+				frequencies.put(data[i], new Integer(1));
+			}
+		}
+		
+		//	Print map entries
+//		Set mapSet = frequencies.entrySet();
+//		Iterator iterator = mapSet.iterator();
+//		while(iterator.hasNext()) {
+//			Map.Entry mapEntry = (Map.Entry)iterator.next();
+//			System.out.println(mapEntry.getKey() + ": " + mapEntry.getValue());
+//		}
+		
+		return frequencies;
+	}
+	
+	public static String tableAsString(int[] data) {
+		String table = "";
+		return table;
+	}
+	
+	public static double mean(int[] data) {
+		double mean = 0.0;
+		
+		mean = (double)sum(data) / data.length;
+		return mean;
+	}
+	
+	public static double median(int[] data) {
+		double median = 0.0;
+		
+		Arrays.sort(data);
+		int middle = data.length / 2;
+		
+		median += data[middle];
+		if(data.length % 2 == 0) {
+			median += data[middle - 1];
+			median = median / 2.0;
+		}
+		
+		return median;
+	}
+	
+	public static int mode(int[] data) {
+		int mode = 0;
+		
+		HashMap<Integer, Integer> values = frequencies(data);
+		Set mapSet = values.entrySet();
+		Iterator iterator = mapSet.iterator();
+		while(iterator.hasNext()) {
+			Map.Entry mapEntry = (Map.Entry)iterator.next();
+			if((Integer)mapEntry.getValue() > mode)
+				mode = (Integer)mapEntry.getKey();
+		}
+		
+		return mode;
+	}
+	
+	public static double variance(int[] data) {
+		double variance = 0.0;
+		int value;
+		double mean = mean(data);
+		
+		HashMap<Integer, Integer> frequencies = frequencies(data);
+		Set mapSet = frequencies.entrySet();
+		Iterator iterator = mapSet.iterator();
+		while(iterator.hasNext()) {
+			Map.Entry mapEntry = (Map.Entry)iterator.next();
+			value = (Integer)mapEntry.getKey();
+			variance += (Integer)mapEntry.getValue() * (value - mean) * (value - mean);
+		}
+		
+		variance = variance / (data.length - 1);
+		return variance;
+	}
+	
+	public static double stdDev(int[] data) {
+		double stdDev = 0.0;
+		
+		stdDev = sqrt(variance(data));
+		
+		return stdDev;
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		//	Read set size and allocate memory for data
+		int n = in.nextInt();
+		int[] data = new int[n];
+		//	Read data
+		for(int i = 0; i < n; i++) {
+			data[i] = in.nextInt();
+		}
+		
+		System.out.println(tableAsString(data));
+		System.out.printf("X- = %.4f\n", mean(data));
+		System.out.printf("X~ = %.4f\n", median(data));
+		System.out.println("X^ = " + (int)mode(data));
+		System.out.printf("s2 = %.4f\n", variance(data));
+		System.out.printf("s  = %.4f\n", stdDev(data));
+	}
+}
